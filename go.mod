98ed1be8689b4ced78627cae0f3ff46c2025fb12
Irbe Krumina <irbekrm@gmail.com>
2020-10-25 10:36:30 +0000

    Adds functionality to pass a custom interval to the repo watcher

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/irbekrm/notify
 go 1.15
 
 require (
-	github.com/google/go-github/v32 v32.1.0 // indirect
+	github.com/google/go-github/v32 v32.1.0
+	github.com/jbeda/go-wait v0.0.0-20160603031512-5fadd5b02ba6
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.7.1 // indirect
 )
--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -16,7 +16,7 @@ const defaultInterval time.Duration = time.Minute * 5
 
 type Client struct {
 	startTime time.Time
-	seen      map[int64]bool
+	seen      map[int]bool
 	interval  time.Duration
 	repo      string
 	owner     string
@@ -24,15 +24,31 @@ type Client struct {
 	ghClient  *github.Client
 }
 
-func NewClient(repo string, owner string, reciever receiver.Notifier) *Client {
-	return &Client{
+func NewClient(repo string, owner string, reciever receiver.Notifier, opts ...option) *Client {
+	c := &Client{
 		startTime: time.Now(),
 		interval:  defaultInterval,
 		repo:      repo,
 		owner:     owner,
 		reciever:  reciever,
-		seen:      make(map[int64]bool),
+		seen:      make(map[int]bool),
 		ghClient:  github.NewClient(nil),
+	}
+	c.applyOptions(opts...)
+	return c
+}
+
+type option func(*Client)
+
+func (c *Client) applyOptions(opts ...option) {
+	for _, o := range opts {
+		o(c)
+	}
+}
+
+func WatchInterval(interval time.Duration) option {
+	return func(c *Client) {
+		c.interval = interval
 	}
 }
 
@@ -42,29 +58,31 @@ func (c *Client) WatchAndTell(wg *sync.WaitGroup) {
 }
 
 func (c *Client) watchAndTellFunc() {
+	log.Printf("checking repo %s/%s for new issues...", c.owner, c.repo)
 	ctx := context.TODO()
 	issues, _, err := c.ghClient.Issues.ListByRepo(ctx, c.owner, c.repo, &github.IssueListByRepoOptions{Since: c.startTime})
 	if err != nil {
 		log.Fatalf("could not list issues: %v", err)
 	}
 	for _, i := range issues {
-		id := i.GetID()
-		if !c.seen[id] {
-			issue := issue{id: id, repo: c.repo, owner: c.owner, url: i.GetHTMLURL(), title: i.GetTitle()}
+		number := i.GetNumber()
+		if !c.seen[number] {
+			log.Printf("found new issue (#%d) in %s/%s", number, c.owner, c.repo)
+			issue := issue{number: number, repo: c.repo, owner: c.owner, url: i.GetHTMLURL(), title: i.GetTitle()}
 			c.reciever.Notify(fmt.Sprintf("%s", issue))
-			c.seen[id] = true
+			c.seen[number] = true
 		}
 	}
 }
 
 type issue struct {
-	id    int64
-	repo  string
-	owner string
-	url   string
-	title string
+	number int
+	repo   string
+	owner  string
+	url    string
+	title  string
 }
 
 func (i issue) String() string {
-	return fmt.Sprintf("New issue in repo %s/%s: %s\n Id: %d, link %s", i.owner, i.repo, i.title, i.id, i.url)
+	return fmt.Sprintf("New issue in repo %s/%s!\nIssue #%d: %s\n %s", i.owner, i.repo, i.number, i.title, i.url)
 }
