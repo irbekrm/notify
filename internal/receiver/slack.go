6ce8965867d86436be1f426fc613ab563e9340e2
Irbe Krumina <irbekrm@gmail.com>
2020-10-30 05:05:57 +0000

    Removes the unused Slack measage header and options

--- internal/receiver/slack.go
+++ internal/receiver/slack.go
@@ -9,29 +9,12 @@ import (
 )
 
 type slack struct {
-	webhookUrl    string
-	messageHeader string
+	webhookUrl string
 }
 
-type slackOption func(*slack)
-
-func (s *slack) applyOptions(opts ...slackOption) {
-	for _, o := range opts {
-		o(s)
-	}
-}
-
-func MessageHeader(msg string) slackOption {
-	return func(s *slack) {
-		s.messageHeader = msg
-	}
-}
-
-func NewSlackReceiver(webhookUrl string, opts ...slackOption) (Notifier, error) {
+func NewSlackReceiver(webhookUrl string) (Notifier, error) {
 	s := &slack{}
-	s.messageHeader = "New GitHub issue"
 	s.webhookUrl = webhookUrl
-	s.applyOptions(opts...)
 	return s, nil
 }
 
